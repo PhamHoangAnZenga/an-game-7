6d01bee3e8d183159ab5b60b331753032ebc3d1c
KNSBA <phamhoangan@zenga.com.vn>
2026-08-17 11:41:51 +0700

    add button fade

--- DOTweenProject/Assets/Project/Scripts/SceneController.cs
+++ DOTweenProject/Assets/Project/Scripts/SceneController.cs
@@ -1,22 +1,43 @@
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SceneController : MonoBehaviour
 {
+    [Header("Objects")]
     [SerializeField] Rigidbody _objA;
-    [SerializeField] Transform _objB;    
-    [SerializeField] float _time;
-    [SerializeField] float _bonus;
+    [SerializeField] Transform _objB;
+    [SerializeField] float _moveTime;
+    [SerializeField] float _delayTime;
+    
+    [Header("Buttons")]
+    [SerializeField] private Button button1;
+    [SerializeField] private CanvasGroup _buttonGroup;
 
-    public void OnClick(Vector3 target)
+    public void Active()
     {
+        button1.interactable = false;
+
         Sequence sequence = DOTween.Sequence();
 
-        Vector3 pos = target + (target - transform.position).normalized * _bonus;
+        Vector3 target = _objB.position + (_objB.position - _objA.transform.position).normalized ;
 
-        sequence.Append(_objA.DOMove(pos, _time).SetSpeedBased()
-                .SetEase(Ease.Linear) );
+        sequence.Append(_objA.DOMove(target, _moveTime));
 
-        sequence.Append(_objA.DOMove(target, _time) );
+        sequence.Append(_objA.DOMove(_objB.position, 1f));
+
+        sequence.AppendInterval(_delayTime);
+
+        sequence.Append(_objB.DOScale(2, 1f));
+
+        sequence.Append(_objB.DOScale(1, 1f));
+
+        sequence.Append(_buttonGroup.DOFade(0, 1f));
+        
+        sequence.OnComplete(() =>
+        {
+            _buttonGroup.interactable = false;
+            _buttonGroup.blocksRaycasts = false;
+        });
     }
 }
